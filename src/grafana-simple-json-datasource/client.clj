fb782812f4ce464f653bde6a6e1bdd85865d9bd9
p3r7 <11557146+p3r7@users.noreply.github.com>
2020-11-27 10:48:36 +0100

    Fix bad query targets format, make helpers privates, inst convertion

--- src/grafana-simple-json-datasource/client.clj
+++ src/grafana-simple-json-datasource/client.clj
@@ -8,8 +8,17 @@
   (:use [slingshot.slingshot :only [try+]]))
 
 
-(declare parse-map-output
-         http-request format-inst-for-query)
+(declare
+ ;; helper parsers
+ parse-search-output
+ metrics-body-parse
+ metrics-body-parse-one
+ ;; time helpers
+ format-inst-for-query
+ maybe-convert-timestamp
+ timestamp->inst
+ ;; HTTP helpers
+ http-request)
 
 
 
@@ -20,7 +29,6 @@
   Valid values:
   - `::http-client`: raw response from `clj-http.client`, good for debugging
   - `::body`: HTTP body parsing into a clojure data structure
-  - `::data`: \"data\" part of the prometheus response
   - `::best`: only the most sensible data for each endpoint (default)"
   ::best)
 
@@ -52,16 +60,35 @@
   [conn & {:keys [target]}]
   (http-request http-client/post conn "/search"
                 {:target (str target)}
-                parse-map-output))
+                parse-search-output))
 
+(defn- parse-search-output
+  [output]
+  (into {} (map #(vector (get % "value") (get % "text")) output)))
 
 
 ;; QUERY: METRICS
 
-(defn metrics-body-parse-one [query-res]
+(defn query
+  [conn targets from to
+   & {:keys [interval limit]}]
+  (let [targets (mapv #(assoc {:type "timeserie"} :target %) targets)
+        from (format-inst-for-query from)
+        to (format-inst-for-query to)]
+    (http-request http-client/post conn "/query"
+                  {:targets targets
+                   :range {:from from
+                           :to   to}}
+                  metrics-body-parse)))
+
+(defn- metrics-body-parse [query-res-list]
+  (mapcat metrics-body-parse-one query-res-list))
+
+(defn- metrics-body-parse-one [query-res]
   (let [
         ;; legends (get body "legends")
-        timestamps (get query-res "times")
+        timestamps (->> (get query-res "times")
+                        (map maybe-convert-timestamp))
         series-list (get query-res "data")]
     (map
      (fn [series]
@@ -74,21 +101,6 @@
           (into {}
                 (map #(vector %1 %2) timestamps values))}))
      series-list)))
-
-(defn metrics-body-parse [query-res-list]
-  (mapcat metrics-body-parse-one query-res-list))
-
-(defn query
-  [conn targets from to
-   & {:keys [interval limit]}]
-  (let [targets (mapv #(assoc {:type "timeserie"} :target %) targets)
-        from (format-inst-for-query from)
-        to (format-inst-for-query to)]
-    (http-request http-client/post conn "/query"
-                  {:targets (str/join "," targets)
-                   :range {:from from
-                           :to   to}}
-                  metrics-body-parse)))
 
 
 
@@ -121,7 +133,16 @@
 
 ;; UTILS: TIME
 
-(defn format-inst
+(defn- maybe-convert-timestamp [ts]
+  (if convert-result
+    (timestamp->inst ts)
+    ts))
+
+(defn- timestamp->inst [ts]
+  ;; REVIEW: `clojure.instant/parse-timestamp` might be a better fit for this but I struggle to find a working example for this use-case.
+  (java.sql.Timestamp. (* ts 1000)))
+
+(defn- format-inst
   "Convert INST into string according to FORMAT-STR.
 
   Unless specified, INST is assumed to be in UTC.
@@ -134,45 +155,39 @@
         f (t/formatter format-str)]
     (t/format f t)))
 
-(defn format-inst-for-query
+(defn- format-inst-for-query
   "Convert INST into a ISO 8601 string, as expected by the Grafana Simple JSON Datasource HTTP API."
   [inst]
   (format-inst inst :iso-zoned-date-time))
 
 
 
-;; UTILS: REUSABLE PARSING
-
-(defn parse-map-output
-  [output]
-  (into {} (map #(vector (get % "value") (get % "text")) output)))
-
-
-
 ;; UTILS: HTTP REQUESTS WRAPPERS
 
-(defn http-request [http-method conn endpoint & [body parse-fn]]
-(let [url (str (:url conn) endpoint)
-      body-str (when body
-                 (json/write-value-as-string body))
-      client-params {:accept :json
-                     :content-type (when body :json)
-                     :body body-str}
-      client-params (into {} (remove nil? client-params))
-      raw-resp (http-method url client-params)]
-  (if (= content-level ::http-client)
-    raw-resp
-    (let [body (-> raw-resp
-                   :body
-                   json/read-value)]
-      (case content-level
-        ::body
-        body
+(defn- http-request [http-method conn endpoint & [body parse-fn]]
+  (let [url (str (:url conn) endpoint)
+        body-str (when body
+                   (json/write-value-as-string body))
+        client-params {:accept :json
+                       :content-type (when body :json)
+                       :body body-str}
+        client-params (into {} (remove nil? client-params))
+        raw-resp (http-method url client-params)
+        ]
+    (if (= content-level ::http-client)
+      raw-resp
+      (let [body (-> raw-resp
+                     :body
+                     json/read-value)]
+        (case content-level
+          ::body
+          body
 
-        ::best
-        (if parse-fn
-          (parse-fn body)
-          body)
+          ::best
+          (if (and convert-result
+                   parse-fn)
+            (parse-fn body)
+            body)
 
-        (throw (ex-info "Unexpected `content-level`" {:ex-type ::unexpected-content-level,
-                                                      :input content-level})))))))
+          (throw (ex-info "Unexpected `content-level`" {:ex-type ::unexpected-content-level,
+                                                        :input content-level})))))))
